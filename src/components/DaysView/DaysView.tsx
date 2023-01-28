3f962a0233d3ac31cac2e2d4723260eb19ab1621
etherbits <nika.qvrivishviliPC@gmail.com>
2023-01-28 20:37:23 +0400

    fixing css module import typo

--- src/components/DaysView/DaysView.tsx
+++ src/components/DaysView/DaysView.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import styles from "./DAysVIew.module.css";
+import styles from "./DaysView.module.css";
 import dayjs, { Dayjs } from "dayjs";
 
 interface Props {
